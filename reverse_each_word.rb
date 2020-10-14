426774c31ed0c9a7dc54bd057c8034e920d827a5
Keith Williams <thestreetkw@hotmail.com>
2020-10-14 18:07:32 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+def reverse_each_word(string)
+array = string.split
+  array.each do |element|
+  element.reverse << array 
+end
+array.join("")
+
+end 
+ 
+ #  def square_array(array)
+ # new_array = []
+  # array.each do |element|
+  #  new_array << element * element
+ # end
+ # new_array
+# end 
